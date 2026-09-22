d2f55ebd8cfb9a7945ab5bf0e5705daf5faf5cd7
kiritolionesi-boop <kiritolionesi@gmail.com>
2026-09-22 14:04:13 +0300

    Создание базового меню.

---
+++ app.js
@@ -0,0 +1,12 @@
+let a = 0, b = 0;
+
+function showMenu() {
+    console.log("=== МЕНЮ ===");
+    console.log("1. Ввести два числа");
+    console.log("2. Выполнить сложение");
+    console.log("3. Выполнить вычитание");
+    console.log("4. Выполнить деление");
+    console.log("5. Возвести число в степень");
+}
+
+showMenu();
